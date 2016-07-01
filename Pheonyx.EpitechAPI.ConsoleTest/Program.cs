1de970cc09752d07e4ff90ae2847abad41ce6780
Alexandre NICOLAIE <alexandre.nicolaie@gmail.com>
2016-07-01 22:49:47 +0200

    [3%] - Utility Tools - Add findRow /!\
    Add findRown algorithm
    
    /!\ No error handler in this function, just algorithm (without optimization)

--- Pheonyx.EpitechAPI.ConsoleTest/Program.cs
+++ Pheonyx.EpitechAPI.ConsoleTest/Program.cs
@@ -8,32 +8,36 @@ using Pheonyx.EpitechAPI;
 
 namespace Pheonyx.EpitechAPI.ConsoleTest
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             string json = @"{'API-local-config':{'API-dbName':'PLANNING.DB','API-modules':[{'Name':'PLANNING','Url':'{EPITECH}/planning/load?format=json&start={START}&end={END}'}]},'#PLANNING':{'Planning':['',{'Name':'acti_title','Title':'title','Start':'start','End':'end','Year':'scolaryear','Semester':'semester','Module':{'Name':'titlemodule','Code':'codemodule','Instance':'codeinstance'},'Location':{'City':'instance_location','Room':{'Name':'room.code','Type':'room.type','Seat':'room.seats'},},'Profs':['prof_inst',{'Type':'type','Login':'login','Name':'title','Picture':'picture'}],'IsRdv':'is_rdv','Type':{'Name':'type_title','Code':'type_code'},'NbStudents':'total_students_registered','NbMaxStudents':'nb_max_students_projet','NbGroup':'nb_group','NbHours':'nb_hours','PlanningStart':'allowed_planning_start','PlanningEnd':'allowed_planning_end','Dates':'dates','IsAvailable':'module_available','IsRegistred':'module_registered','IsDone':'past','AllowedRegistred':'allow_register','EventRegistred':'event_registered','Project':'project','Register':{'Group':'rdv_group_registered','Individual':'rdv_indiv_registered','Student':'register_student','Prof':'register_prof','Month':'register_month',}}]}}";
-            JToken data = JToken.Parse(json);
+            string user = @"{'login':'nicola_s','title':'alexandre nicolaieditclairville','internal_email':'alexandre.nicolaieditclairville @epitech.eu','lastname':'nicolaieditclairville','firstname':'alexandre','userinfo':{'birthday':{'value':'28\/ 02','adm':true,'public':true},'address':{'value':'None'},'city':{'value':'Castanet Tolosan'},'country':{'value':'France'},'job':{'value':'Post'},'birthplace':{'value':'France'},'telephone':{'value':'0123456789'},'twitter':{'value':'twoot'},'facebook':{'value':'facebook.com'},'googleplus':{'value':'google+'},'website':{'value':'Nope'},'email':{'value':'Nope @gmail.com'},'poste':{'value':'Post?'}},'referent_used':false,'picture':'https:\/\/cdn.local.epitech.eu\/userprofil\/profilview\/nicola_s.jpg','picture_fun':null,'scolaryear':'2015','promo':2019,'semester':4,'uid':49130,'gid':32019,'location':'FR\/TLS','documents':'vrac\/nicola_s','userdocs':'\/u\/epitech_2019\/nicola_s\/cu','shell':null,'close':false,'ctime':'2014-08-17 17:00:01','mtime':'2016-06-28 03:11:42','id_promo':'396','id_history':'170925','course_code':'bachelor\/classic','semester_code':'B4','school_id':'1','school_code':'epitech','school_title':'epitech','old_id_promo':'381,380,383,386,385','old_id_location':'14','rights':{},'invited':false,'studentyear':2,'admin':false,'editable':true,'locations':null,'groups':[{'title':'Toulouse','name':'Toulouse','count':608}],'events':[],'credits':119,'gpa':[{'gpa':'3.14','cycle':'bachelor'}],'spice':null,'nsstat':{'active':9,'idle':0,'out_active':0,'out_idle':0,'nslog_norm':40}}";
+
+            JToken jsonData = JToken.Parse(json);
+            JToken jsonUser = JToken.Parse(user);
             Console.WriteLine("-----------");
-            Console.WriteLine(data);
+            Console.WriteLine(jsonData);
             Console.WriteLine("-----------");
-            Console.WriteLine(data["Test"] == null);
+            Console.WriteLine(jsonData["Test"] == null);
             Console.WriteLine("-----------");
-            Console.WriteLine(Utility.JsonTools.setVariable(data["API-local-config"]["API-modules"][0]["Url"], new Dictionary<string, string>()
+            Console.WriteLine(Utility.JsonTools.setVariable(jsonData["API-local-config"]["API-modules"][0]["Url"], new Dictionary<string, string>()
             {
                 { "EPITECH", "https://intra.epitech.eu" },
                 { "START", "2016-01-01" },
                 { "END", "2016-02-01" },
             }
             ));
-            Console.WriteLine(Utility.JsonTools.accessPath("API-local-config.API-modules.[0].Url", data));
-            Console.WriteLine(Utility.JsonTools.setVariable(Utility.JsonTools.accessPath("API-local-config.API-modules.[0].Url", data), new Dictionary<string, string>()
+            Console.WriteLine(Utility.JsonTools.accessPath("API-local-config.API-modules.0.Url", jsonData));
+            Console.WriteLine(Utility.JsonTools.setVariable(Utility.JsonTools.accessPath("API-local-config.API-modules.0.Url", jsonData), new Dictionary<string, string>()
             {
                 { "EPITECH", "https://intra.epitech.eu" },
                 { "START", "2016-01-01" },
                 { "END", "2016-02-01" },
             }
               ));
+            Console.WriteLine(Utility.JsonTools.findRow("gpa[cycle=bachelor].gpa", jsonUser));
             Console.ReadKey();
         }
     }
--- Pheonyx.EpitechAPI/Utility/JsonTools.cs
+++ Pheonyx.EpitechAPI/Utility/JsonTools.cs
@@ -9,11 +9,10 @@ namespace Pheonyx.EpitechAPI.Utility
     {
         static public JValue setVariable(JToken jValue, Dictionary<string, string> dVar)
         {
-            string sValue = jValue.ToString();
+            string sValue;
 
             if (jValue == null)
                 throw new System.ArgumentNullException("jValue");
-            //Todo: Gestion des messages d'erreurs
             if (!(jValue is JValue))
                 throw new System.ArgumentException("incorrect type (must be JValue)", "jValue");
             if (dVar == null)
@@ -21,14 +20,16 @@ namespace Pheonyx.EpitechAPI.Utility
             if (dVar.Count == 0)
                 return (jValue.Value<JValue>());
 
+            sValue = jValue.ToString();
+
             foreach (var pVal in dVar)
                 sValue = sValue.Replace("{" + pVal.Key + "}", pVal.Value);
             return (new JValue(sValue));
         }
+
         static public JToken accessPath(string sPath, JToken jRoot)
         {
             JToken jTemporary;
-            Regex rgxEngine = new Regex(@"[(\d+)]");
 
             if (sPath == null)
                 throw new System.ArgumentNullException("sPath");
@@ -43,17 +44,53 @@ namespace Pheonyx.EpitechAPI.Utility
                         try { jTemporary = jTemporary[sNode]; }
                         catch { throw new System.ArgumentOutOfRangeException(sNode); }
                         break;
+
                     case JTokenType.Array:
-                        if (!rgxEngine.IsMatch(sNode))
-                            throw new System.ArgumentException(string.Format("'{0}' in '{1}' must be an array index", sNode, sPath));
-                        var iNode = Convert.ToInt32(rgxEngine.Matches(sNode)[0].Value);
+                        int iNode;
+
+                        if (!int.TryParse(sNode, out iNode))
+                            throw new System.ArgumentException(string.Format("'{0}' in '{1}' must be an Int32", sNode, sPath));
                         try { jTemporary = jTemporary[iNode]; }
                         catch { throw new System.IndexOutOfRangeException(); }
                         break;
+
                     default:
                         throw new System.ArgumentOutOfRangeException(sNode);
                 }
             return (jTemporary);
+        }
+
+        static public JToken findRow(JToken jValue, JToken jRoot)
+        {
+            string sValue;
+            Regex engineArguments = new Regex(@"([\w.\[\]]+)\[([\w.]+)=(\w+)\]([\w.\[\]]+)");
+            Match matchFinder;
+            JToken jQuery;
+
+            if (jValue == null)
+                throw new System.ArgumentNullException("jValue");
+            if (!(jValue is JValue))
+                throw new System.ArgumentException("incorrect type (must be JValue)", "jValue");
+            if (jRoot == null)
+                throw new System.ArgumentNullException("jRoot");
+
+            sValue = jValue.ToString();
+            if (!(matchFinder = engineArguments.Match(sValue)).Success || matchFinder.Groups.Count != 5)
+                throw new System.ArgumentException();
+            if ((jQuery = accessPath(matchFinder.Groups[1].Value, jRoot)).Type != JTokenType.Array)
+                throw new System.Exception("Can't access to this");
+            string sComparator = matchFinder.Groups[3].Value;
+            JToken tQuery = null;
+            foreach (JToken jContent in jQuery)
+            {
+                tQuery = jContent;
+                JValue tValue = accessPath(matchFinder.Groups[2].Value, jContent) as JValue;
+                if (tValue == null)
+                    throw new Exception("It's not property");
+                if (tValue.Value.ToString() == matchFinder.Groups[3].Value)
+                    break;
+            }
+            return (accessPath(matchFinder.Groups[4].Value.Remove(0, 1), tQuery));
         }
     }
 }
